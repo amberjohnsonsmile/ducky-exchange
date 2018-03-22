ec5b4e18bcb3fe4972606b179760bea418a4786d
Amber Johnson <amber.johnson.smile@gmail.com>
2018-03-22 14:48:13 -0600

    Add navbar and cards

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Card, CardTitle, Col, Navbar, NavItem} from 'react-materialize';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Navbar brand="Ducky Exchange" className="yellow darken-1" left>
+          <NavItem href="https://bittrex.com/">Bittrex</NavItem>
+          <NavItem href="https://coincap.io/">CoinCap</NavItem>
+          <NavItem href="https://www.kraken.com/">Kraken</NavItem>
+          <NavItem href="https://poloniex.com/">Poloniex</NavItem>
+        </Navbar>
+        <main>
+          <Col m={7} s={12}>
+            <Card
+              horizontal
+              header={<CardTitle image="./assets/louie.png" />}
+              actions={[<a href="#">This is a link</a>]}>
+              <p>
+                I am a very simple card. I am good at containing small bits of
+                information
+              </p>
+            </Card>
+          </Col>
+          <Col m={7} s={12}>
+            <Card
+              horizontal
+              header={<CardTitle image="./assets/dewey.png" />}
+              actions={[<a href="#">This is a link</a>]}>
+              <p>
+                I am a very simple card. I am good at containing small bits of
+                information
+              </p>
+            </Card>
+          </Col>
+          <Col m={7} s={12}>
+            <Card
+              horizontal
+              header={<CardTitle image="./assets/huey.gif" />}
+              actions={[<a href="#">This is a link</a>]}>
+              <p>
+                I am a very simple card. I am good at containing small bits of
+                information
+              </p>
+            </Card>
+          </Col>
+        </main>
       </div>
     );
   }
