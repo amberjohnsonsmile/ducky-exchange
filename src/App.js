f9231a298a8b0d9a115a0cf87d5a75d93c584f18
Amber Johnson <amber.johnson.smile@gmail.com>
2018-03-22 16:10:25 -0600

    Add names and styling to cards

--- src/App.js
+++ src/App.js
@@ -7,41 +7,44 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Navbar brand="Ducky Exchange" className="yellow darken-1" left>
+        <Navbar brand="Ducky Exchange" className="amber lighten-1" left>
           <NavItem href="https://bittrex.com/">Bittrex</NavItem>
           <NavItem href="https://coincap.io/">CoinCap</NavItem>
           <NavItem href="https://www.kraken.com/">Kraken</NavItem>
           <NavItem href="https://poloniex.com/">Poloniex</NavItem>
         </Navbar>
         <main>
-          <Row>
-            <Col m={2} s={12}>
+          <Row className="cards">
+            <Col m={4} s={12}>
+              <Card
+                horizontal
+                header={<CardTitle image="./assets/huey.png" />}
+                actions={[<a href="#">This is a link</a>]}>
+                <h2>Huey</h2>
+                <p>
+                  I am a very simple card. I am good at containing small bits of
+                  information
+                </p>
+              </Card>
+            </Col>
+            <Col m={4} s={12}>
               <Card
                 horizontal
                 header={<CardTitle image="./assets/louie.png" />}
                 actions={[<a href="#">This is a link</a>]}>
+                <h2>Louie</h2>
                 <p>
                   I am a very simple card. I am good at containing small bits of
                   information
                 </p>
               </Card>
             </Col>
-            <Col m={2} s={12}>
+            <Col m={4} s={12}>
               <Card
                 horizontal
                 header={<CardTitle image="./assets/dewey.png" />}
                 actions={[<a href="#">This is a link</a>]}>
-                <p>
-                  I am a very simple card. I am good at containing small bits of
-                  information
-                </p>
-              </Card>
-            </Col>
-            <Col m={2} s={12}>
-              <Card
-                horizontal
-                header={<CardTitle image="./assets/huey.gif" />}
-                actions={[<a href="#">This is a link</a>]}>
+                <h2>Dewey</h2>
                 <p>
                   I am a very simple card. I am good at containing small bits of
                   information
