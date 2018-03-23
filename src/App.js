6e504345598a345e4486513cfd05e6eeef9a38d6
Amber Johnson <amber.johnson.smile@gmail.com>
2018-03-23 09:35:35 -0600

    Add hard-coded info to cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React, {Component} from 'react';
-import {Card, CardTitle, Col, Navbar, NavItem, Row} from 'react-materialize';
+import {
+  Card,
+  CardTitle,
+  Col,
+  Collapsible,
+  CollapsibleItem,
+  Navbar,
+  NavItem,
+  Row
+} from 'react-materialize';
 import logo from './logo.svg';
 import './App.css';
 
@@ -16,40 +25,56 @@ class App extends Component {
         <main>
           <Row className="cards">
             <Col m={4} s={12}>
-              <Card
-                horizontal
-                header={<CardTitle image="./assets/huey.png" />}
-                actions={[<a href="#">This is a link</a>]}>
-                <h2>Huey</h2>
-                <p>
-                  I am a very simple card. I am good at containing small bits of
-                  information
-                </p>
+              <Card horizontal header={<CardTitle image="./assets/huey.png" />}>
+                <h2 className="name">Huey - </h2>
+                <h2 className="currency">Ethereum</h2>
+                <h3>Kraken currently offers the best exchange:</h3>
+                <p>1 Ethereum = 0.061309 Bitcoin</p>
+                <p>For 5 Bitcoin, Donald can buy Huey 81.55 Etehereum.</p>
               </Card>
+              <Collapsible className="address">
+                <CollapsibleItem
+                  header="Huey's Bitcoin Address"
+                  icon="credit_card">
+                  37Pu2KBsn2yEaHfqhwHAXD9BWpQtxUJn8R
+                </CollapsibleItem>
+              </Collapsible>
             </Col>
             <Col m={4} s={12}>
               <Card
                 horizontal
-                header={<CardTitle image="./assets/louie.png" />}
-                actions={[<a href="#">This is a link</a>]}>
-                <h2>Louie</h2>
-                <p>
-                  I am a very simple card. I am good at containing small bits of
-                  information
-                </p>
+                header={<CardTitle image="./assets/louie.png" />}>
+                <h2 className="name">Louie - </h2>
+                <h2 className="currency">Litecoin</h2>
+                <h3>Kraken currently offers the best exchange:</h3>
+                <p>1 Litecoin = 0.0188195 Bitcoin</p>
+                <p>For 5 Bitcoin, Donald can buy Louie 265.68 Litecoin.</p>
               </Card>
+              <Collapsible className="address">
+                <CollapsibleItem
+                  header="Louie's Bitcoin Address"
+                  icon="credit_card">
+                  3E6HuQeSqTtk4i88fTji73oN3NisTzU9R6
+                </CollapsibleItem>
+              </Collapsible>
             </Col>
             <Col m={4} s={12}>
               <Card
                 horizontal
-                header={<CardTitle image="./assets/dewey.png" />}
-                actions={[<a href="#">This is a link</a>]}>
-                <h2>Dewey</h2>
-                <p>
-                  I am a very simple card. I am good at containing small bits of
-                  information
-                </p>
+                header={<CardTitle image="./assets/dewey.png" />}>
+                <h2 className="name">Dewey - </h2>
+                <h2 className="currency">Dash</h2>
+                <h3>Poloniex currently offers the best exchange:</h3>
+                <p>1 Dash = 0.04727 Bitcoin</p>
+                <p>For 5 Bitcoin, Donald can buy Dewey 105.78 Dash.</p>
               </Card>
+              <Collapsible className="address">
+                <CollapsibleItem
+                  header="Dewey's Bitcoin Address"
+                  icon="credit_card">
+                  3BYsxrhEbLuC38YtQi5ZJSTeswFt55Fznn
+                </CollapsibleItem>
+              </Collapsible>
             </Col>
           </Row>
         </main>
