944aa21b327fdbbb830186dcb642009bebaf6f81
Amber Johnson <amber.johnson.smile@gmail.com>
2018-03-23 10:04:54 -0600

    Add footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Col,
   Collapsible,
   CollapsibleItem,
+  Footer,
   Navbar,
   NavItem,
   Row
@@ -24,12 +25,12 @@ class App extends Component {
         </Navbar>
         <main>
           <Row className="cards">
-            <Col m={4} s={12}>
+            <Col l={4} m={6} s={12}>
               <Card horizontal header={<CardTitle image="./assets/huey.png" />}>
                 <h2 className="name">Huey - </h2>
                 <h2 className="currency">Ethereum</h2>
                 <h3>Kraken currently offers the best exchange:</h3>
-                <p>1 Ethereum = 0.061309 Bitcoin</p>
+                <p className="exchange">1 Ethereum = 0.061309 Bitcoin</p>
                 <p>For 5 Bitcoin, Donald can buy Huey 81.55 Etehereum.</p>
               </Card>
               <Collapsible className="address">
@@ -40,14 +41,14 @@ class App extends Component {
                 </CollapsibleItem>
               </Collapsible>
             </Col>
-            <Col m={4} s={12}>
+            <Col l={4} m={6} s={12}>
               <Card
                 horizontal
                 header={<CardTitle image="./assets/louie.png" />}>
                 <h2 className="name">Louie - </h2>
                 <h2 className="currency">Litecoin</h2>
                 <h3>Kraken currently offers the best exchange:</h3>
-                <p>1 Litecoin = 0.0188195 Bitcoin</p>
+                <p className="exchange">1 Litecoin = 0.0188195 Bitcoin</p>
                 <p>For 5 Bitcoin, Donald can buy Louie 265.68 Litecoin.</p>
               </Card>
               <Collapsible className="address">
@@ -58,14 +59,14 @@ class App extends Component {
                 </CollapsibleItem>
               </Collapsible>
             </Col>
-            <Col m={4} s={12}>
+            <Col l={4} m={6} s={12}>
               <Card
                 horizontal
                 header={<CardTitle image="./assets/dewey.png" />}>
                 <h2 className="name">Dewey - </h2>
                 <h2 className="currency">Dash</h2>
                 <h3>Poloniex currently offers the best exchange:</h3>
-                <p>1 Dash = 0.04727 Bitcoin</p>
+                <p className="exchange">1 Dash = 0.04727 Bitcoin</p>
                 <p>For 5 Bitcoin, Donald can buy Dewey 105.78 Dash.</p>
               </Card>
               <Collapsible className="address">
@@ -78,6 +79,43 @@ class App extends Component {
             </Col>
           </Row>
         </main>
+        <Footer
+          className="amber darken-1"
+          copyrights="&copy 2015 Copyright Text"
+          moreLinks={
+            <a className="grey-text text-lighten-4 right" href="#!">
+              More Links
+            </a>
+          }
+          links={
+            <ul>
+              <li>
+                <a className="grey-text text-lighten-3" href="#!">
+                  Link 1
+                </a>
+              </li>
+              <li>
+                <a className="grey-text text-lighten-3" href="#!">
+                  Link 2
+                </a>
+              </li>
+              <li>
+                <a className="grey-text text-lighten-3" href="#!">
+                  Link 3
+                </a>
+              </li>
+              <li>
+                <a className="grey-text text-lighten-3" href="#!">
+                  Link 4
+                </a>
+              </li>
+            </ul>
+          }>
+          <h5 className="white-text">Footer Content</h5>
+          <p className="grey-text text-lighten-4">
+            You can use rows and columns here to organize your footer content.
+          </p>
+        </Footer>
       </div>
     );
   }
